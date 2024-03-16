97b4d669bc499452ba734d4fbba5469963524387
Utkarsh <utkarshpatil.it@gmail.com>
2024-03-16 10:51:20 +0530

    modified settings.py

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -29,7 +29,7 @@ DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 # DEBUG = True
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-ALLOWED_HOSTS = []
+# ALLOWED_HOSTS = []
 
 
 # Application definition
